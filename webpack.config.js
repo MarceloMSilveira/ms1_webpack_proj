9b1e600fe73f0fcfb492c222ca58da44381157e1
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-07-29 14:11:12 -0300

    add html webpack plugin

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [new HtmlWebpackPlugin()],
+};
